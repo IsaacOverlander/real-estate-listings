059a751f1e96076e3b6813e2bad094cfe802ee33
Unknown <Isaac.overlander@gmail.com>
2018-08-17 16:28:45 -0500

    Updated client.js

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,16 @@
-console.log('JS');
+const myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function($routeProvider){
+    $routeProvider.when('/', {
+        templateUrl: 'views/home.html'
+    })
+    .when('/rentals', {
+        templateUrl: 'views/rentals.html',
+        controller: 'RentalController as rc'
+    }).when('/sales', {
+        templateUrl: 'views/sales.html',
+        controller: 'SalesController as sc'
+    }).otherwise({
+        templateUrl: 'views/404.html'
+    });
+});
