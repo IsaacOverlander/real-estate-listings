e0682c5f5d134acc2a4f96e29e6bec5f21baade9
Unknown <Isaac.overlander@gmail.com>
2018-08-19 07:39:26 -0500

    Filled out sales controller

---
+++ server/public/scripts/controllers/sale.controller.js
@@ -0,0 +1,33 @@
+myApp.controller('SaleController', function($http){
+    const vm = this;
+
+    vm.salesList = []
+
+    function getSales(){
+        $http({
+            method: 'GET',
+            url: '/sales'
+        }).then(function(response) {
+            vm.salesList = response.data;
+        }).catch(function(error) {
+            alert('there was an error getting sales');
+            console.log('Error', error);
+        });// End GET
+    }
+
+    vm.deleteListing = function(listing) {
+        console.log('in delete');
+        $http({
+            method: 'DELETE',
+            url: '/sales/' + listing.id
+        }).then(function (response) {
+            console.log('deleted listing');
+            getSales();
+        }).catch(function(error) {
+            alert('error deleting listing');
+            console.log('Error', error);
+        });//End DELETE
+    }
+
+    getSales();
+});
